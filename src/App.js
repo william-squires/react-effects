994c23bd064b35698c4a7be93d663c865cb49512
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-04-25 14:30:47 -0500

    async issues with card

--- src/App.js
+++ src/App.js
@@ -5,29 +5,45 @@ import CardList from './CardList';
 import axios from 'axios';
 
 /** App
- * 
+ *
  * State
  * -cards:
  *     [ {image, value, suit},...]
  * -isLoading: boolean
- *      
+ *
  */
 function App() {
-  const [deck, setDeck] = useState({ deck: null, isLoading: true });
-  const [cards, setCards] = useState([]);
+  const [deck, setDeck] = useState(
+      { deck: null, isLoading: true }
+  );
+  const [cards, setCards] = useState(
+    { cards: [], isLoading: true}
+  );
+  console.log("cards state=", cards.cards);
+  console.log("cards.isLoading=", cards.isLoading);
 
   useEffect(function fetchCardOnClick() {
     async function fetchCard() {
-
+      const resp = await axios.get(`https://deckofcardsapi.com/api/deck/${deck.deck.deckId}/draw/`);
+      const newCard = resp.data.cards[0];
+      const newCards = [...cards.cards, newCard]
+      setCards({cards: newCards, isLoading: false});
+      // setDeck(d => d.deck.remaining = resp.data.remaining);
     }
-  },
+    // if(deck.deck){
+    //   fetchCard();
+    // }
+    if(!cards.isLoading){
+      fetchCard();
+    }
+  },[cards]
   )
 
   /** gets a deck from api on mount */
   useEffect(function fetchDeckOnMount() {
     async function fetchDeck() {
-      const resp = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle")
-      const newDeck = resp.data
+      const resp = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle");
+      const newDeck = resp.data;
       setDeck(
         {
           deck:
@@ -43,10 +59,17 @@ function App() {
     return <div>Loading...</div>
   }
 
+  function handleClick() {
+    console.log("c in clic=", cards);
+    const cardsInClick = cards.cards;
+    setCards({cards: cardsInClick, isLoading: true})
+  }
+
+
   return (
     <div className="App">
-      <button >Draw a card</button>
-      <CardList cards={cards} />
+      <button onClick={handleClick}>Draw a card</button>
+      <CardList cards={cards.cards} />
     </div>
   );
 }
--- src/CardList.js
+++ src/CardList.js
@@ -1,13 +1,14 @@
 import Card from "./Card";
 
-/** Renders a list of cards 
- * 
+/** Renders a list of cards
+ *
  * Props
  * -cards:
  *      [ {image, value, suit},...]
- * 
+ *
 */
 function CardList({ cards }) {
+    console.log("cards in cardList=", cards);
     return (
         <div>
             {cards.map((c, idx) => <Card key={idx} card={c} />)}
